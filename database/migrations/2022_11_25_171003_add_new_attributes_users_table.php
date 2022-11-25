8b3d9e1e5489ac68599e30701ccf3f3da99e04c8
Sergio Adonay López Mejía <sergio.lopez@ISRI.CENTRAL>
2022-11-25 13:40:43 -0600

    inicializador

---
+++ database/migrations/2022_11_25_171003_add_new_attributes_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('web_site')->nullable();
+            $table->string('nick_name')->unique();
+            $table->text('presentation')->nullable();
+            $table->boolean('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('web_site');
+            $table->dropColumn('nick_name');
+            $table->dropColumn('presentation');
+            $table->dropColumn('status')->default(0);
+        });
+    }
+};
---
+++ database/migrations/2022_11_25_172943_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_send');
+            $table->unsignedBigInteger('user_recive');
+
+            $table->foreign('user_send')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_recive')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+};
