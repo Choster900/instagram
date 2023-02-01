d6718fd3008e737dc95578c38425c588987292d5
SergioCokie <16adonaysergio@gmail.com>
2023-01-31 21:20:35 -0600

    bug de buscar amigos arreglado

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { ref } from 'vue';
-import { Inertia } from '@inertiajs/inertia';
-import { Head, Link } from '@inertiajs/inertia-vue3';
-import ApplicationMark from '@/Components/ApplicationMark.vue';
-import Banner from '@/Components/Banner.vue';
-import Dropdown from '@/Components_bySergio/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import AuthenticationCardLogo from '@/Components_bySergio/AuthenticationCardLogo.vue';
-import { def } from '@vue/shared';
-import axios from 'axios';
+import { ref } from "vue";
+import { Inertia } from "@inertiajs/inertia";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import ApplicationMark from "@/Components/ApplicationMark.vue";
+import Banner from "@/Components/Banner.vue";
+import Dropdown from "@/Components_bySergio/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import AuthenticationCardLogo from "@/Components_bySergio/AuthenticationCardLogo.vue";
+import { def } from "@vue/shared";
+import axios from "axios";
 
 defineProps({
     title: String,
@@ -19,20 +19,21 @@ defineProps({
 const showingNavigationDropdown = ref(false);
 
 const switchToTeam = (team) => {
-    Inertia.put(route('current-team.update'), {
-        team_id: team.id,
-    }, {
-        preserveState: false,
-    });
+    Inertia.put(
+        route("current-team.update"),
+        {
+            team_id: team.id,
+        },
+        {
+            preserveState: false,
+        }
+    );
 };
 
 const logout = () => {
-    Inertia.post(route('logout'));
+    Inertia.post(route("logout"));
 };
-
-
 </script>
-
 
 <template>
     <div>
@@ -55,56 +56,53 @@ const logout = () => {
 
                             <!-- Buscando usuarios Dropdown -->
                             <div class="ml-96 relative mt-1">
-                                <Dropdown align="center" width="100" overflow="overflow-y-auto" maxheight="300">
-                                    <template #trigger>
-                                        <div class="pt-2 relative mx-auto text-gray-600">
-                                            <input v-model="buscando" @keyup="buscarUsuario"
-                                                class="border-2 border-gray-300 bg-white w-100 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                                                type="search" placeholder="Buscar amigos">
-                                            <span class="absolute right-0 top-0 mt-5 mr-4">
-                                                <svg class="text-gray-600 h-4 w-4 fill-current"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1"
-                                                    x="0px" y="0px" viewBox="0 0 56.966 56.966"
-                                                    style="enable-background:new 0 0 56.966 56.966;"
-                                                    xml:space="preserve" width="512px" height="512px">
-                                                    <path
-                                                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                                                </svg>
-                                            </span>
-                                        </div>
-                                    </template>
 
-                                    <template #content>
-                                        <div v-if="buscando.length > 0">
-                                            <Link class="flex items-center py-2 px-3 hover:bg-gray-100"
-                                                v-for="(usuario, i) in usuarios" :key="i"
-                                                :href="'/profile/' + usuario.nick_name">
-                                            <img :src="usuario.profile_photo_url"
-                                                class="rounded-full w-9 h-9 object-cover">
-                                            <div class="ml-2">
-                                                <span class="block font-bold text-gray-700 text-sm">{{
-                                                    usuario.nick_name
-                                                }}</span>
-                                                <span class="block font-light text-gray-400 text-sm">{{
-                                                    usuario.name
-                                                }}</span>
-                                            </div>
-                                            </Link>
-                                        </div>
-                                        <div v-else>
-                                            <div class="flex items-center py-2 px-3 hover:bg-gray-100">
-                                                <span
-                                                    class="block font-bold text-gray-700 text-sm cursor-default">Buscar
-                                                    amigos</span>
-                                            </div>
+                                <div class="pt-2 relative mx-auto text-gray-600">
+                                    <input v-model="buscando" @input="buscarUsuario"
+                                        class="border-2 border-gray-300 bg-white w-100 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                                        type="search" placeholder="Buscar amigos" />
+
+                                    <span class="absolute right-0 top-0 mt-5 mr-4">
+                                        <svg class="text-gray-600 h-4 w-4 fill-current"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px"
+                                            y="0px" viewBox="0 0 56.966 56.966" style="
+                                                        enable-background: new 0
+                                                            0 56.966 56.966;
+                                                    " xml:space="preserve" width="512px" height="512px">
+                                            <path
+                                                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                                        </svg>
+                                    </span>
+                                </div>
+
+                                <div v-if="activo" class="drop-shadow-2xl bg-white rounded-md">
+                                    <div v-if="usuarios.length > 0">
+                                        <Link class="flex items-center py-2 px-3 hover:bg-gray-100"
+                                            v-for="(usuario, i) in usuarios" :key="i" :href="
+                                                '/profile/' +
+                                                usuario.nick_name
+                                            ">
+                                        <img :src="
+                                            usuario.profile_photo_url
+                                        " class="rounded-full w-9 h-9 object-cover" />
+                                        <div class="ml-2">
+                                            <span class="block font-bold text-gray-700 text-sm">{{
+                                                usuario.nick_name
+                                            }}</span>
+                                            <span class="block font-light text-gray-400 text-sm">{{
+                                                usuario.name
+                                            }}</span>
                                         </div>
 
-                                    </template>
+                                        </Link>
+                                    </div>
 
-                                </Dropdown>
+                                    <Link class="flex items-center py-2 px-3 hover:bg-gray-100" v-else>
+                                    <p class="text-center font-semibold text-sm">No se encontraron amigos</p>
+                                    </Link>
+                                </div>
                             </div>
-
                         </div>
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- settings Dropdown -->
@@ -115,30 +113,39 @@ const logout = () => {
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                                 </svg>
-
                                 </Link>
                             </div>
-                            <div class=" ml-3 relative">
+                            <div class="ml-3 relative">
                                 <Dropdown align="right" width="48">
                                     <template #trigger>
-                                        <button v-if="$page.props.jetstream.managesProfilePhotos"
+                                        <button v-if="
+                                            $page.props.jetstream
+                                                .managesProfilePhotos
+                                        "
                                             class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                            <img class="h-8 w-8 rounded-full object-cover"
-                                                :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name">
+                                            <img class="h-8 w-8 rounded-full object-cover" :src="
+                                                $page.props.user
+                                                    .profile_photo_url
+                                            " :alt="$page.props.user.name" />
                                         </button>
 
                                         <span v-else class="inline-flex rounded-md">
                                             <button type="button"
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                                <img :src="$page.props.user.profile_photo_url"
-                                                    class="h-10 w-10 rounded-full" alt="">
+                                                <img :src="
+                                                    $page.props.user
+                                                        .profile_photo_url
+                                                " class="h-10 w-10 rounded-full" alt="" />
                                             </button>
                                         </span>
                                     </template>
 
                                     <template #content>
                                         <!-- Account Management -->
-                                        <DropdownLink :href="'/profile/' + $page.props.user.nick_name">
+                                        <DropdownLink :href="
+                                            '/profile/' +
+                                            $page.props.user.nick_name
+                                        ">
                                             perfil
                                         </DropdownLink>
 
@@ -146,8 +153,10 @@ const logout = () => {
                                             Configuracion
                                         </DropdownLink>
 
-                                        <DropdownLink v-if="$page.props.jetstream.hasApiFeatures"
-                                            :href="route('api-tokens.index')">
+                                        <DropdownLink v-if="
+                                            $page.props.jetstream
+                                                .hasApiFeatures
+                                        " :href="route('api-tokens.index')">
                                             API Tokens
                                         </DropdownLink>
 
@@ -163,21 +172,15 @@ const logout = () => {
                                 </Dropdown>
                             </div>
                         </div>
-
                     </div>
                 </div>
-
-
-
             </nav>
-
             <!-- Page Heading -->
             <header v-if="$slots.header" class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
             </header>
-
             <!-- Page Content -->
             <main>
                 <div class="max-w-7x1 min-w-7x1 mx-auto py-14">
@@ -190,26 +193,27 @@ const logout = () => {
     </div>
 </template>
 
-
 <script>
 export default {
-
     data() {
         return {
-
             usuarios: [],
-            buscando: '',
-
-        }
+            buscando: "",
+            activo: false
+        };
     },
     methods: {
         async buscarUsuario() {
-            await axios.get("/search/" + this.buscando)
-                .then(response => {
-                    this.usuarios = response.data
-                })
-        }
+            await axios.get("/search/" + this.buscando).then((response) => {
+                this.usuarios = response.data;
+            });
+        },
     },
-
-}
+    watch: {
+        buscando(nuevoValor, valorAnterior) {
+            this.buscando.length > 0 ? this.activo = true : this.activo = false
+            console.log("El nombre pasó de ser %s a %s", valorAnterior, nuevoValor);
+        }
+    }
+};
 </script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,38 +1,31 @@
 <template>
     <div class="max-w-sm sm:max-w-md md:max-w-lg w-full">
-
-
         <button @click="changeStateShowCreatePost"
-            class="w-full mt-5 text-center bg-blue-500 rounded text-white py-2 outline-none focus:outline-none hover:bg-blue-600">Agregar
-            publicacion
+            class="w-full mt-5 text-center bg-blue-500 rounded text-white py-2 outline-none focus:outline-none hover:bg-blue-600">
+            Agregar publicacion
         </button>
 
-
-        <!-- ESTO ES LA PARTE DE RECORRIDO DE PUBLICACIONES -->
-        <div v-if="(posts.length > 0)">
-            <post-component-vue v-for="(publicaciones, i) in posts" :key="i" :post="publicaciones" @dataPost="setPost"/>
+        <div v-if="posts.length > 0">
+            <post-component-vue v-for="(publicaciones, i) in posts" :key="i" :post="publicaciones"
+                @dataPost="setPost" />
         </div>
         <div v-else class="text-3x1 text-center mt-4">No hay publicaciones</div>
-        <!--// ESTO ES LA PARTE DE RECORRIDO DE PUBLICACIONES -->
 
-
-        <ModalPostVue 
-            :show="showModalPost" 
-            :publicacion="unicPost"
-            @mostrandoElModal="changeStateModalPost" 
-        />
-
+        <ModalPostVue :show="showModalPost" :publicacion="unicPost" @mostrandoElModal="changeStateModalPost" />
 
         <ModalVue :show="showModalCreate" @close="changeStateShowCreatePost">
-            <div class="p-5">
-                <div class="">
+            <div class="p-5" id="modal_publicar_post">
+                <div>
                     <input v-model="text" type="text"
                         class="w-full outline-none focus:outline-none p-2 rounded appearance-none border-none"
-                        placeholder="En que estas pensando">
+                        placeholder="En que estas pensando" />
 
                     <div class="my-5">
-                        <img class="rounded-lg" v-if="url" :src="url"
-                            style="max-width: 100%; max-height: 400px; margin: 0 auto;">
+                        <img class="rounded-lg" v-if="url" :src="url" style="
+                                max-width: 100%;
+                                max-height: 400px;
+                                margin: 0 auto;
+                            " />
                     </div>
                     <div class="flex justify-end">
                         <button @click="selecImage"
@@ -42,110 +35,107 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
-
                         </button>
                         <input @change="fileChange" type="file" name="image" id="image"
-                            accept="image/gif,image/jpeg,image/png,image/jpg,image" style="display: none ;">
+                            accept="image/gif,image/jpeg,image/png,image/jpg,image" style="display: none" />
                     </div>
                     <div class="text-red-500 pd-2 mt-5">
                         {{ error_img }}
                     </div>
                 </div>
-                <button v-if="(text.length > 0 && img != null)" @click="createPost"
+                <button v-if="text.length > 0 && img != null" @click="createPost"
                     class="w-full mt-5 text-center bg-blue-500 rounded text-white py-2 outline-none focus:outline-none hover:bg-blue-600">
                     Publicar
                 </button>
             </div>
         </ModalVue>
-
-
     </div>
 </template>
 
 <script>
-
-import PostComponentVue from '../Components_bySergio/PostComponent.vue';
-import ModalVue from '../Components/Modal.vue';
-import ModalPostVue from '../Components_bySergio/ModalPost.vue';
+import PostComponentVue from "../Components_bySergio/PostComponent.vue";
+import ModalVue from "../Components/Modal.vue";
+import ModalPostVue from "../Components_bySergio/ModalPost.vue";
 
 export default {
     data() {
-        return {//estas son las variables
+        return {
+            //estas son las variables
             showModalCreate: false,
             showModalPost: false,
             url: null,
             img: null,
-            text: '',
+            text: "",
             posts: [],
             unicPost: [],
             error_img: null,
-        }
+        };
     },
     components: {
         PostComponentVue,
         ModalVue,
-        ModalPostVue
+        ModalPostVue,
     },
     methods: {
-        async getPost() {//obteniendo todos las publicaciones
-            await axios.get("/list-post").then(response => {
-                this.posts = response.data
-            })
+        async getPost() {
+            //obteniendo todos las publicaciones
+            await axios.get("/list-post").then((response) => {
+                this.posts = response.data;
+            });
         },
         changeStateShowCreatePost() {
-            this.showModalCreate = !this.showModalCreate
+            this.showModalCreate = !this.showModalCreate;
         },
         changeStateModalPost() {
-            this.showModalPost = !this.showModalPost
-
+            this.showModalPost = !this.showModalPost;
         },
         fileChange(e) {
-            let file = e.target.files[0]
-            this.img = file
-            this.url = URL.createObjectURL(file)
-
+            let file = e.target.files[0];
+            this.img = file;
+            this.url = URL.createObjectURL(file);
         },
         selecImage() {
-            document.getElementById("image").click()
+            document.getElementById("image").click();
         },
         async createPost() {
+            const formData = new FormData();
+            formData.append("image", this.img);
+            formData.append("text", this.text);
 
-            const formData = new FormData()
-            formData.append('image', this.img)
-            formData.append('text', this.text)
-
-            await axios.post('/create-post', formData, {
-
-                headers: {
-                    'Content-Type': 'multipart/form-data'//sin esto no se puede envari la iamgen
-                }
-
-            }).then((res) => {
-                this.posts.unshift(res.data)
-                this.resentForm()
-            }).catch(Error => {
-                if (Error.response.status === 422) {
-                    this.error_img = Error.response.data.message
-                    setTimeout(() => {
-                        this.error_img = null
-                    }, 5000);
-                }
-            })
+            await axios
+                .post("/create-post", formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data", //sin esto no se puede envari la iamgen
+                    },
+                })
+                .then((res) => {
+                    this.posts.unshift(res.data);
+                    this.resentForm();
+                })
+                .catch((Error) => {
+                    if (Error.response.status === 422) {
+                        this.error_img = Error.response.data.message;
+                        setTimeout(() => {
+                            this.error_img = null;
+                        }, 5000);
+                    }
+                });
         },
-        resentForm() {//reseteando formulario
-            this.showModal = false
-            this.url = null
-            this.img = null
-            this.text = ''
+        resentForm() {
+            //reseteando formulario
+            this.showModal = false;
+            this.url = null;
+            this.img = null;
+            this.text = "";
         },
-        setPost(publicacion){
-            this.showModalPost = !this.showModalPost
-            this.unicPost = publicacion
-        }
+        setPost(publicacion) {
+            this.showModalPost = !this.showModalPost;
+            this.unicPost = publicacion;
+        },
     },
-    created() {//esto es como lo de jquery que se ejecuta al cargar
-        this.getPost()
-    }
-}
-
+    created() {
+        //esto es como lo de jquery que se ejecuta al cargar
+        this.getPost();
+    },
+};
 </script>
